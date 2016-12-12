63ea405d70e6b6abff98045fc44393e03a0d04ba
Reina.Zwarts <reinazwarts@gmail.com>
2016-12-12 17:23:45 +0100

    publish to pages

---
+++ src/js/toggle-guidelines.js
@@ -0,0 +1,21 @@
+﻿;(function ($) {
+	'use strict';
+
+	var toggleGuidelines = function () {
+
+		$('.js-toggle-guidelines').on('click', function () {
+			$('body').toggleClass('debug');
+		});
+
+
+	};
+
+
+
+	function init() {
+		toggleGuidelines();
+	}
+
+	$(document).ready(init);
+
+})(jQuery);
